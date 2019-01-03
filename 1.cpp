b5d21f5cf626db5561a833d976eecc9c573a93db
toxa81 <toxa81@gmail.com>
2019-01-03 15:38:15 +0100

    fix

--- 1.cpp
+++ 1.cpp
@@ -5,6 +5,7 @@ int main(int argn, char** argv)
     int nd = acc::num_devices();
     printf("number of devices: %i\n", nd);
     for (int i = 0; i < nd; i++) {
+        printf("device: %i\n", i);
         acc::print_device_info(i);
     }
 
